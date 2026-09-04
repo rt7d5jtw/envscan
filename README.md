047ddb39c128c5b073d8e90132f512801bebe72b
Iia <rt7d5jtw@protonmail.com>
2026-09-04 13:11:15 +0300

    updated Annotations and README.md

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ The parser includes support for:
 
 The parser code is intentionally written using **ECMAScript 3 (ES3) syntax** (var, prototypes, plain objects) and wrapped in a Universal Module Definition (UMD) to attempt to guarantee portability across modern Node.js environments, web browsers, and legacy JavaScript engines like SpiderMonkey and Rhino.
 
-⚠️ **Injecting Environment Variables:** Because this module attemtps to be platform-agnostic, it does not automatically read system environment variables. To enable variable expansion using the host's environment, you must manually inject them before parsing:
+⚠️ **Injecting Environment Variables:** Because this module attemtps to be platform-agnostic, it does not automatically read system environment variables. This is used for interpolated strings from the existing host environment variables. To enable variable expansion using the host's environment, you must manually add them before parsing:
 
 ```javascript
 var EnvParser = require('./lib/index.js');
--- tests/es3check.js
+++ tests/es3check.js
@@ -1,3 +1,7 @@
+/*
+ * This file is to test the compatibility with ECMAScript 3 by parsing it with
+ * JavaScript parser, Acorn, with ecmaVersion set to '3'.
+ */
 var acorn = require('../vendor/acorn.js');
 var fs    = require('fs');
 
@@ -6,9 +10,31 @@ try {
   var code = fs.readFileSync(source, 'utf8');
 
   acorn.parse(code, {
+    /*
+     * ecmaVersion: Indicates the ECMAScript version to parse. Can be a number,
+     * either in year (2022) or plain version number (6) form, or "latest" (the
+     * latest the library supports). This influences support for strict mode,
+     * the set of reserved words, and support for new syntax features.
+     */
     ecmaVersion: 3,
+    /*
+     * sourceType: Indicate the mode the code should be parsed in. Can be
+     * either "script", "module" or "commonjs". This influences global strict
+     * mode and parsing of import and export declarations.
+     */
     sourceType: 'script',
-    allowReserved: true,
+    /*
+     * allowReserved: If false, using a reserved word will generate an error.
+     * Defaults to true for ecmaVersion 3, false for higher versions. When
+     * given the value "never", reserved words and keywords can also not be
+     * used as property names (as in Internet Explorer's old parser).
+     */
+    allowReserved: false,
+    /*
+     * locations: When true, each node has a loc object attached with start and
+     * end subobjects, each of which contains the one-based line and zero-based
+     * column numbers in {line, column} form. Default is false.
+     */
     locations: false
   });
 
